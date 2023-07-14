9e69998c6b72a46f2de1e5f640d15f47d74c8fae
rquinterosv <rafael.quinterosv@gmail.com>
2023-07-14 14:42:42 -0400

    Starting the project

---
+++ client/photos_books/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Welcome!</h3>
+        <h1>Photos & Books</h1>
+        <h4>Share you motivation with</h4>
+        <h4>Nature pictures and reference of books</h4>
+        <button>Enter</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
